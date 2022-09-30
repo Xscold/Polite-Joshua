a57480e1b1723faa3a36f39f0ca4530c1d79b27c
Xscold <103187013+Xscold@users.noreply.github.com>
2022-10-01 07:52:40 +0800

    readme edited

--- raite2022-api/src/routes/product/product.js
+++ raite2022-api/src/routes/product/product.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const PRODUCT_CONTROLLER = require("../../controller/product/product")
+const fileStorageEngine = require("../../middleware/upload")
 
-router.post("/add-product", PRODUCT_CONTROLLER.ADD_PRODUCT)
+const upload = multer({storage: fileStorageEngine})
+
+router.post("/add-product", upload.single('image'), PRODUCT_CONTROLLER.ADD_PRODUCT)
 router.post("/search-product-by-name/:productName", PRODUCT_CONTROLLER.SEARCH_PRODUCT_BY_NAME)
 router.put("/update-product/:id", PRODUCT_CONTROLLER.UPDATE_PRODUCT)
 router.delete("/delete-product/:id", PRODUCT_CONTROLLER.DELETE_PRODUCT)
